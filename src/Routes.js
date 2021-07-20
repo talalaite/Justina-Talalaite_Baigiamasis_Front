489ca8139a0e19d9512f6fdfe0405236f23aeee7
talalaite <justina.talalaite@gmail.com>
2021-07-20 22:29:25 +0300

    routes

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Home, Login, Products, Register } from "./pages";
+
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;

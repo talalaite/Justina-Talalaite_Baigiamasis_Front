c35487de03e95fea080c349eb8618e0a0897d2f9
talalaite <justina.talalaite@gmail.com>
2021-07-26 00:44:28 +0300

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+
+import { GiHouse, GiBee } from 'react-icons/gi';
+import { ImPhone } from 'react-icons/im';
+import { MdEmail } from 'react-icons/md';
+import { FaRegCopyright } from 'react-icons/fa';
+
 import * as S from './Footer.style';
 
 const Footer = () => (
   <>
     <S.Footer>
-      <h1>Footer</h1>
+      <S.FooterWrapper>
+        <S.Title>contacts</S.Title>
+
+        <S.Ul>
+          <S.ListItems>
+            <ImPhone />
+            +37064352759
+          </S.ListItems>
+
+          <S.ListItems>
+            <MdEmail />
+            bitenumedus@gmail.com
+          </S.ListItems>
+
+          <S.ListItems>
+            <GiHouse />
+            M. Jankaus g. 1, Bitėnų medus
+          </S.ListItems>
+
+          <S.ListItems>
+            <GiBee />
+            Bityno paso Nr. 63BPR-36
+          </S.ListItems>
+        </S.Ul>
+
+        <S.CopyRight>
+          <FaRegCopyright />
+          justina talalaite
+        </S.CopyRight>
+      </S.FooterWrapper>
     </S.Footer>
   </>
 );
--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -4,3 +4,30 @@ export const Footer = styled.div`
   color: #ffffff;
   margin-top: auto;
 `;
+
+export const FooterWrapper = styled.div`
+  padding: 2rem;
+  text-align: center;
+`;
+
+export const Title = styled.h3`
+  color: #eac000;
+  text-transform: uppercase;
+  margin-bottom: 0;
+`;
+
+export const Ul = styled.div`
+  margin-bottom: 1rem;
+`;
+
+export const ListItems = styled.li`
+  list-style: none;
+  padding-top: 0.7rem;
+  font-size: 0.8rem;
+`;
+
+export const CopyRight = styled.span`
+  color: #eac000;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+`;
